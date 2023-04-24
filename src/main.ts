b27e9aff1f539fd9ec200289537405baff0db6a2
clavijo99 <bclavijogomez@gmail.com>
2023-04-24 02:18:52 -0500

    add tailwind

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import { setupLayouts } from 'virtual:generated-layouts'
 import generatedRoutes from 'virtual:generated-pages'
 import App from './App.vue'
+import './styles/tailwind.css'
 
 import {
   i18n,
